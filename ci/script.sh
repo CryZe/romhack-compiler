6c0cbcd1cb70c9d5c6060a0eb593ca88414430d7
Christopher Serr <christopher.serr@gmail.com>
2018-08-24 03:44:55 +0200

    Add Continuous Integration

---
+++ ci/script.sh
@@ -0,0 +1,23 @@
+# This script takes care of testing your crate
+
+set -ex
+
+main() {
+    cat > Cross.toml <<EOF
+[target.x86_64-unknown-linux-gnu]
+image = "cryze/x86_64-unknown-linux-gnu-romhack-compiler"
+[target.i686-unknown-linux-gnu]
+image = "cryze/i686-unknown-linux-gnu-romhack-compiler"
+EOF
+
+    cross rustc -p romhack-patcher --target $TARGET --release -- -C link-arg=-Wl,-rpath,'$ORIGIN'
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
